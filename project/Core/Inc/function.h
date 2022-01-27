b56537c39b236c72d1ebc316d62aaa35e4f6d7ad
Marcin279 <marcin.biela12@gmail.com>
2022-01-27 18:17:54 +0100

    obsluga cyklicznego nadawania

---
+++ project/Core/Inc/function.h
@@ -0,0 +1,13 @@
+/*
+ * function.h
+ *
+ *  Created on: Jan 22, 2022
+ *      Author: jakub
+ */
+
+#ifndef INC_FUNCTION_H_
+#define INC_FUNCTION_H_
+
+void wyswietlacz (int cyfra, int port);
+
+#endif /* INC_FUNCTION_H_ */
